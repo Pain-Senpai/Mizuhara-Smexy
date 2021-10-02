715a7e48ab34077db717f50f558e2431a0826b8c
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-10-02 15:49:01 +0530

    Update mongo.py

--- MizuharaSmexyBot/mongo.py
+++ MizuharaSmexyBot/mongo.py
@@ -9,7 +9,7 @@ from MizuharaSmexyBot.conf import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "DaisyX"
+MONGO_DB = "TeamSmexy"
 
 
 client = MongoClient()
